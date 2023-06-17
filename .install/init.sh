a32bb0ba948cbbcedaca2b50e11f906a0ecbcb42
ponbac <root@ponbac.xyz>
2023-06-17 13:08:47 +0200

    mv

--- .install/init.sh
+++ .install/init.sh
@@ -2,22 +2,6 @@
 
 declare -r SSH_DIR="${SSH_DIR:-"$HOME/.ssh"}"
 
-# Create SSH key if it doesn't exist and copy it to clipboard
-if ! [ -f "$SSH_DIR/id_rsa.pub" ]; then
-    echo "SSH key not found. Generating one..."
-    ssh-keygen
-fi
-
-echo "Your public SSH key is:"
-echo
-cat "$SSH_DIR/id_rsa.pub"
-echo
-echo "Copying SSH key to clipboard..."
-cat "$SSH_DIR/id_rsa.pub" | xclip -selection clipboard
-echo "SSH key copied to clipboard! Paste it into your GitHub account."
-echo "Press any key to continue when you are done..."
-read -n 1
-
 # Add Neovim repository and update system
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
@@ -44,6 +28,22 @@ nvm install node
 
 # Refresh shell
 exec bash
+
+# Create SSH key if it doesn't exist and copy it to clipboard
+if ! [ -f "$SSH_DIR/id_rsa.pub" ]; then
+    echo "SSH key not found. Generating one..."
+    ssh-keygen
+fi
+
+echo "Your public SSH key is:"
+echo
+cat "$SSH_DIR/id_rsa.pub"
+echo
+echo "Copying SSH key to clipboard..."
+cat "$SSH_DIR/id_rsa.pub" | xclip -selection clipboard
+echo "SSH key copied to clipboard! Paste it into your GitHub account."
+echo "Press any key to continue when you are done..."
+read -n 1
 
 # Ask user if they now want to run the ecovim installer
 echo "Do you want to run the ecovim installer now? [y/n]"
